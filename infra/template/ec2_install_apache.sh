40e42c94288d31499176cc26020b1d7b6f8311cb
jojwayoung <jojwayoung421@gmail.com>
2026-03-25 13:44:27 +0900

    FIXED: typo for pytho application folder path

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -6,7 +6,7 @@ yes | sudo apt install python3 python3-pip
 git clone https://github.com/jojwayoung/devops-project1-python-mysql-db.git
 sleep 20
 # shellcheck disable=SC2164
-cd python-mysql-db-proj-1
+cd devops-project1-python-mysql-db
 pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &
